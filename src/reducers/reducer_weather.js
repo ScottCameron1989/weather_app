cde83e240e894ed2fa4ae917ca84eadcdb3394a5
ScottCameron1989 <scottcameron1989@gmail.com>
2018-06-04 23:56:50 -0400

    fixed 404 response crashing site

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -2,10 +2,15 @@ import { FETCH_WEATHER } from '../actions/index';
 
 export default function(state = [],action)
 {
-  switch(action.type)
-  {
+  if(action.error) {
+    return state;
+  }
+
+  switch (action.type) {
     case FETCH_WEATHER:
-      return [action.payload.data, ...state];
+      return [
+        action.payload.data, ...state
+      ];
   }
 
   return state;
